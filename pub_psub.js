6a35d625dc9b824356df559c76ab4d2cf9e43346
dinos <dinostheo@gmail.com>
2012-06-18 17:46:17 +0200

    psusbscribe
    read a number of channels stored in a set and psubscribe to them

---
+++ pub_psub.js
@@ -0,0 +1,37 @@
+var express = require('express');
+
+var app = module.exports = express.createServer();
+
+app.use(express.static(__dirname + '/public'));
+
+const redis = require('redis');
+const redisClient = redis.createClient();
+
+const io = require('socket.io');
+
+if (!module.parent) {
+    app.listen(8080);
+    console.log("Express server listening on port %d", app.address().port)
+
+    const socket  = io.listen(app);
+
+    socket.on('connection', function(client) {
+        const subscribe = redis.createClient();
+        redisClient.smembers('redis:channels', function (err, channels){
+			for (var i = 0; i < channels.length; i++) {
+				subscribe.psubscribe(channels[i]);
+			}
+		});
+
+        subscribe.on("pmessage", function(pattern, channel, message) {
+            client.send(message);
+        });
+
+        client.on('message', function(msg) {
+        });
+
+        client.on('disconnect', function() {
+            subscribe.quit();
+        });
+    });
+} 
